8078e9ab7b2a657fd890a02b3a10aa328d8ae510
Arman Abkar <armanw@outlook.com>
2020-07-30 13:50:39 +0430

    Add files via upload

---
+++ server.ts
@@ -0,0 +1,28 @@
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import {
+  getNotes,
+  createNote,
+  getSingleNote,
+  updateNote,
+  deleteNote,
+} from "./routes.ts";
+
+const router = new Router();
+
+router
+  .get("/", (ctx) => {
+    ctx.response.body = "Welcome to notes API";
+  })
+  .get("/notes", getNotes)
+  .get("/notes/:id", getSingleNote)
+  .post("/notes", createNote)
+  .put("/notes/:id", updateNote)
+  .delete("/notes/:id", deleteNote);
+
+const app = new Application();
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+const port = 5000;
+app.listen({ port });
+console.log(`Server is running on port ${port}`);
